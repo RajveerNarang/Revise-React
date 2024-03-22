a44e57d9b6242a97b83d87a45532a0152d54fe0f
RajveerNarang <dhanurrajveer@gmail.com>
2024-03-22 19:58:22 +0530

    nested structure creation

--- App.js
+++ App.js
@@ -1,3 +1,29 @@
+/**
+* <div id="parent">
+<div id ="child">
+<h1>I am h1 tag</h1>
+</div>
+</div>
+*
+*
+* 
+**/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am an h1 tag")
+  )
+);
+
 const heading = React.createElement("h1", { id: "heading" }, "Revising React");
+console.log(heading); //object h1 tag is object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+root.render(parent);
+// object is a react element
+//render converts object in tag.
+//react element -> obj -> becomes HTML
